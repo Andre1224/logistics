92ce64f3450a9f868b0370b46cf00842b1031fff
guoshuai <506867200@qq.com>
2021-12-08 10:35:46 +0800

    增加svg图标管理

--- src/icons/index.js
+++ src/icons/index.js
@@ -4,6 +4,11 @@ import SvgIcon from '@/components/BackstageManagerComponents/SvgIcon'// svg comp
 // register globally
 Vue.component('svg-icon', SvgIcon)
 
-const req = require.context('./svg', false, /\.svg$/)
+// const req = require.context('./svg', false, /\.svg$/)
+const req = require.context('./svg/default', false, /\.svg$/)
+const req2 = require.context('./svg/indexIcon', false, /\.svg$/)
+const req3 = require.context('./svg/back', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
+requireAll(req2)
+requireAll(req3)
